627fdac6b445ca60e0ea3d1f541185560879f273
Bumju-Kim <xeno1492@gmail.com>
2022-02-06 18:38:58 +0900

    baekjoon 1120

--- test.c
+++ test.c
@@ -1,13 +1,25 @@
 #include <stdio.h>
 
 int main(){
-    int a, b, c1, c2, c3;
-    scanf("%d %d", &a, &b);
-    c1 = b/100;
-    c2 = b%100/10;
-    c3 = b%10;
-
-    printf("%d\n%d\n%d\n%d", a*c3, a*c2, a*c1, a*b);
-
+    int num;
+    int a, b, c=190, cnt=0;
+    
+    scanf("%d", &num);
+    
+    
+    while(1){
+        if(c == num)
+            break;
+        else if(c == 190)
+            c = num;
+        cnt++;
+        a = c % 10;
+        b = c / 10;
+        
+        c = (c%10)*10 + (a+b)%10;
+ //       printf("%d %d %d\n", a, b, c);
+    }
+    printf("%d", cnt);
+    
     return 0;
 }
